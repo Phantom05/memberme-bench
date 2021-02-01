10be70e5768a8a687c0c2651d4c1ec052cc6aee2
phantom05 <monster2jy@gmail.com>
2021-02-01 23:37:21 +0900

    set axios
    set axios

--- src/pages/News.tsx
+++ src/pages/News.tsx
@@ -1,19 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import { useDispatch, useSelector } from 'react-redux';
 import { MainHeader } from '@components/header';
 import { PlainTemplate } from '@components/template';
 import { Page } from '@pages/index';
 import { mapper } from '@lib/mapper';
 import { MainFooter } from '@components/footer';
 import Container from '@material-ui/core/Container';
-const News = () => (
-  <Page title={`${mapper.brand.name} - News`}>
-    <PlainTemplate header={<MainHeader />} footer={<MainFooter />}>
-      <Container maxWidth="lg">
-        <div>News Page</div>
-      </Container>
-    </PlainTemplate>
-  </Page>
-);
+import axtios from 'axios';
+
+const News = () => {
+  console.log('hmm..');
+  const state = useSelector((state) => state);
+  const dispatch = useDispatch();
+  console.log(state, 'state');
+  useEffect(() => {
+    axtios('https://jsonplaceholder.typicode.com/todos/1').then(({ data }) => {
+      console.log(data);
+    });
+  }, []);
+  return (
+    <Page title={`${mapper.brand.name} - News`}>
+      <PlainTemplate header={<MainHeader />} footer={<MainFooter />}>
+        <Container maxWidth="lg">
+          <div>News Page</div>
+        </Container>
+      </PlainTemplate>
+    </Page>
+  );
+};
 
 export default News;
